5d02621f0deb70ea172acd32264f0dc6834a928a
EginRyzen <muhammadaliagil@gmail.com>
2024-02-03 08:11:42 +0700

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function Square(){
+ return <button className='square'>X</button>
+}
+
+function App() {
+
+  return (
+    <>
+      <Square/>
+    </>
+  )
+}
+
+export default App
